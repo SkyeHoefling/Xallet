99a998c360846b0028528f9a8113841659e5e647
Andrew Hoefling <andrew@hoefling.me>
2020-04-02 20:58:28 -0400

    Initial commit

---
+++ Xallet/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xallet.Services;
+using Xallet.Views;
+
+namespace Xallet
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
