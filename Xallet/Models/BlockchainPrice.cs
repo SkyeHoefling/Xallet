357050548a0c774e94a4f892829608cea0467f82
Andrew Hoefling <andrew@hoefling.me>
2020-04-04 17:04:04 -0400

    Added blockchain price for bitcoin values

---
+++ Xallet/Models/BlockchainPrice.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace Xallet.Models
+{
+    [JsonObject]
+    public class BlockchainPrice
+    {
+        [JsonProperty("15m")]
+        public double FifteenMinutesPrice { get; set; }
+
+        [JsonProperty("last")]
+        public double LastPrice { get; set; }
+
+        [JsonProperty("buy")]
+        public double BuyPrice { get; set; }
+
+        [JsonProperty("sell")]
+        public double SellPrice { get; set; }
+
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+    }
+}
--- Xallet/Services/BlockchainService.cs
+++ Xallet/Services/BlockchainService.cs
@@ -11,6 +11,7 @@ namespace Xallet.Services
     public class BlockchainService : IBlockchainService
     {
         private const string API_Accounts = "https://blockchain.info/balance?active={0}";
+        private const string API_Price = "https://blockchain.info/ticker";
         private const double TokenFactor = 100000000;
         public async Task<double[]> GetAccountBalanceAsync(params string[] addresses)
         {
@@ -48,29 +49,27 @@ namespace Xallet.Services
 
         public async Task<double> GetCryptoValueAsync()
         {
-            await Task.Delay(0);
-            return 6600;
-            //try
-            //{
-            //    using (var client = new HttpClient())
-            //    {
-            //        var apiKey = AppSettingsManager.Settings["EtherScan:ApiToken"];
-            //        var route = string.Format(API_Price, apiKey);
-            //
-            //        var result = await client.GetAsync(route);
-            //        if (result.IsSuccessStatusCode)
-            //        {
-            //            var content = await result.Content.ReadAsStringAsync();
-            //            var etherscanResponse = JsonConvert.DeserializeObject<EtherscanResponse<EtherscanPrice>>(content);
-            //            return etherscanResponse.Result.EtherUSD;
-            //        }
-            //    }
-            //}
-            //catch
-            //{
-            //}
-            //
-            //return 0;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var result = await client.GetAsync(API_Price);
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var content = await result.Content.ReadAsStringAsync();
+                        var json = JObject.Parse(content);
+
+                        var findUSD = json.SelectToken("USD");
+                        if (findUSD != null)
+                            return findUSD.ToObject<BlockchainPrice>().LastPrice;
+                    }
+                }
+            }
+            catch
+            {
+            }
+            
+            return 0;
         }
     }
 }
